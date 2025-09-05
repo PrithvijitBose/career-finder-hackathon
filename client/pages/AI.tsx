5527717a895218dc8956423d805ed9bd233d3695
PrithvijitBose <prithvijitb18@gmail.com>
2025-09-05 19:23:19 +0530

    Added our lates AI feature

---
+++ client/pages/AI.tsx
@@ -0,0 +1,9 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <Spline scene="https://prod.spline.design/jvqbpAADue0LwAk2/scene.splinecode" />
+    </div>
+  );
+}
